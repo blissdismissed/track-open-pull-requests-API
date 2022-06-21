73c115ff44f2c43e2d15d00da5524d7dd6630974
ajautz@gmail.com <ajautz@gmail.com>
2022-06-21 15:34:36 -0400

    add needed headers to request to GitHub API

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const routes = require('./routes');
 
+const enableCors = require('./middlewares');
+
 const app = express();
+
+app.use(enableCors);
 
 app.use('/github_api', routes);
 
--- controller.js
+++ controller.js
@@ -6,8 +6,12 @@ function basicPage(req, res) {
 
 async function getZen(req, res) {
   const response = await fetch('https://api.github.com/zen', {
-    method: "GET"
+    method: "GET",
+    headers: {
+      'User-Agent': 'blissdismissed-track-open-pull-requests-API'
+    }
   });
+  console.log(response);
   res.send(response);
 }
 
