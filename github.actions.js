9afd71626a9468ef79bea56cf7e6f6334f3cc4ab
ajautz@gmail.com <ajautz@gmail.com>
2022-06-22 14:57:58 -0400

    add number of commits value for each open pull request

--- github.actions.js
+++ github.actions.js
@@ -1,49 +1,68 @@
-const fetch = require('node-fetch');
-const options = require('./util/options');
+const fetch = require("node-fetch");
+const options = require("./util/options");
 
- 
 async function pullData(user, reponame) {
   const setOptions = options("/repos/" + user + "/" + reponame + "/pulls");
 
-  const response = await fetch(setOptions.baseUrl+setOptions.hostName+setOptions.path, {
-    method: "GET",
-    headers: setOptions.headers
-  });
+  const response = await fetch(
+    setOptions.baseUrl + setOptions.hostName + setOptions.path,
+    {
+      method: "GET",
+      headers: setOptions.headers
+    }
+  );
 
   const data = await response.json();
   console.log(data);
 
-  const output = await Promise.all (data.map(async element => {
-    const startPage = 1
-    const commitsPerPage = 100;
-    let numberOfCommits = 0;
-  
-    const commitOptions = options("/repos/" + user + "/" + reponame + 
-          "/pulls/" + element.number + "/commits?per_page=" + commitsPerPage + "&page=" + startPage);
-  
-        const getCommitData = await fetch(commitOptions.baseUrl + commitOptions.hostName + commitOptions.path, {
-          method: "GET",
-          headers: commitOptions.headers
-        });
-  
+  const output = await Promise.all(
+    data.reverse().map(async element => {
+      let startPage = 1;
+      const commitsPerPage = 1;
+      let numberOfCommits = 0;
+      let commitData = [0];
+
+      while (commitData.length > 0) {
+        const commitOptions = options(
+          "/repos/" +
+            user +
+            "/" +
+            reponame +
+            "/pulls/" +
+            element.number +
+            "/commits?per_page=" +
+            commitsPerPage +
+            "&page=" +
+            startPage
+        );
+
+        const getCommitData = await fetch(
+          commitOptions.baseUrl + commitOptions.hostName + commitOptions.path,
+          {
+            method: "GET",
+            headers: commitOptions.headers
+          }
+        );
+
         commitData = await getCommitData.json();
         console.log("Length: ", commitData.length);
-        numberOfCommits = commitData.length;
-      
-  
-    return {
-      id: element.id,
-      number: element.number,
-      title: element.title,
-      author: element.user.login,
-      commit_count: numberOfCommits
-    };
+        numberOfCommits += commitData.length;
+        console.log("After: ", numberOfCommits);
+        startPage += 1;
+      }
 
-  })
+      return {
+        id: element.id,
+        number: element.number,
+        title: element.title,
+        author: element.user.login,
+        commit_count: numberOfCommits
+      };
+    })
   );
   return output;
 }
 
 module.exports = {
   pullData: pullData
-}
+};
