efba2fce0c1d995ea803b32c8b75297ba43a1345
ajautz@gmail.com <ajautz@gmail.com>
2022-06-23 13:50:07 -0400

    cleaning up controller, removing console.logs, removing previous test action.

--- controller.js
+++ controller.js
@@ -5,29 +5,14 @@ function basicPage(req, res) {
   res.send('Boom, GitHub API!');
 }
 
-async function getZen(req, res) {
-  const response = await fetch('https://api.github.com/zen', {
-    method: "GET",
-    headers: {
-      'Accept' : 'application/vnd.github.v3+json',
-      'User-Agent': 'blissdismissed'
-    }
-  });
-  console.log(response);
-  res.send(response);
-}
-
 async function fetchPulls(req,res) {
   const user = req.params.user;
   const reponame = req.params.reponame;
-  console.log(user,reponame);
   const githubOutputData = await github.pullData(user, reponame);
-  console.log(githubOutputData);
   res.json(githubOutputData);
 }
 
 module.exports = {
   basicPage: basicPage,
-  getZen: getZen,
   fetchPulls: fetchPulls
 }
