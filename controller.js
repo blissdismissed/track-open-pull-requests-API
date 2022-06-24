533f8ce2f62b3831a04295ef814dd12614c2d48c
ajautz@gmail.com <ajautz@gmail.com>
2022-06-24 08:48:47 -0400

    refactoring github action function to make it more modular and testable

--- controller.js
+++ controller.js
@@ -1,4 +1,3 @@
-const fetch = require('node-fetch');
 const github = require('./github.actions');
 
 function basicPage(req, res) {
--- github.actions.js
+++ github.actions.js
@@ -3,7 +3,7 @@ const options = require("./util/options");
 
 async function pullData(user, reponame) {
   const setOptions = options("/repos/" + user + "/" + reponame + "/pulls");
-
+  // console.log("here");
   const response = await fetch(
     setOptions.baseUrl + setOptions.hostName + setOptions.path,
     {
@@ -11,56 +11,102 @@ async function pullData(user, reponame) {
       headers: setOptions.headers
     }
   );
-
+  // console.log("Response: ", response);
   const data = await response.json();
-  console.log(data);
+  // console.log("Data:", data);
 
+  const output = outputApiData(data, user, reponame);
+
+  return output;
+}
+
+async function outputApiData(pulldata, user, reponame) {
   const output = await Promise.all(
-    data.reverse().map(async element => {
-      let startPage = 1;
-      const commitsPerPage = 1;
-      let numberOfCommits = 0;
-      let commitData = [0];
+    pulldata.reverse().map(async element => {
+      // let startPage = 1;
+      // const commitsPerPage = 1;
+      // let numberOfCommits = 0;
+      // let commitData = [0];
 
-      while (commitData.length > 0) {
-        const commitOptions = options(
-          "/repos/" +
-            user +
-            "/" +
-            reponame +
-            "/pulls/" +
-            element.number +
-            "/commits?per_page=" +
-            commitsPerPage +
-            "&page=" +
-            startPage
-        );
+      const commitCount = await getCommitCount(element, user, reponame);
+      console.log("CommitCount: ", commitCount);
 
-        const getCommitData = await fetch(
-          commitOptions.baseUrl + commitOptions.hostName + commitOptions.path,
-          {
-            method: "GET",
-            headers: commitOptions.headers
-          }
-        );
+      // while (commitData.length > 0) {
+      //   const commitOptions = options(
+      //     "/repos/" +
+      //       user +
+      //       "/" +
+      //       reponame +
+      //       "/pulls/" +
+      //       element.number +
+      //       "/commits?per_page=" +
+      //       commitsPerPage +
+      //       "&page=" +
+      //       startPage
+      //   );
 
-        commitData = await getCommitData.json();
-        console.log("Length: ", commitData.length);
-        numberOfCommits += commitData.length;
-        console.log("After: ", numberOfCommits);
-        startPage += 1;
-      }
+      //   const getCommitData = await fetch(
+      //     commitOptions.baseUrl + commitOptions.hostName + commitOptions.path,
+      //     {
+      //       method: "GET",
+      //       headers: commitOptions.headers
+      //     }
+      //   );
+
+      //   commitData = await getCommitData.json();
+      //   console.log("Length: ", commitData.length);
+      //   numberOfCommits += commitData.length;
+      //   console.log("After: ", numberOfCommits);
+      //   startPage += 1;
+      // }
 
       return {
         id: element.id,
         number: element.number,
         title: element.title,
         author: element.user.login,
-        commit_count: numberOfCommits
+        commit_count: commitCount
       };
     })
   );
+
   return output;
+}
+
+async function getCommitCount(element, user, reponame) {
+  let startPage = 1;
+  const commitsPerPage = 1;
+  let numberOfCommits = 0;
+  let commitData = [0];
+  while (commitData.length > 0) {
+    const commitOptions = options(
+      "/repos/" +
+        user +
+        "/" +
+        reponame +
+        "/pulls/" +
+        element.number +
+        "/commits?per_page=" +
+        commitsPerPage +
+        "&page=" +
+        startPage
+    );
+
+    const getCommitData = await fetch(
+      commitOptions.baseUrl + commitOptions.hostName + commitOptions.path,
+      {
+        method: "GET",
+        headers: commitOptions.headers
+      }
+    );
+
+    commitData = await getCommitData.json();
+    console.log("Length: ", commitData.length);
+    numberOfCommits += commitData.length;
+    console.log("After: ", numberOfCommits);
+    startPage += 1;
+  }
+  return numberOfCommits;
 }
 
 module.exports = {
