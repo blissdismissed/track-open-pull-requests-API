be91782b1749718a19fff62aa223088428608dd9
ajautz@gmail.com <ajautz@gmail.com>
2022-06-24 08:54:52 -0400

    github actions refactored into three different functions

--- github.actions.js
+++ github.actions.js
@@ -1,9 +1,10 @@
 const fetch = require("node-fetch");
 const options = require("./util/options");
 
+
 async function pullData(user, reponame) {
   const setOptions = options("/repos/" + user + "/" + reponame + "/pulls");
-  // console.log("here");
+
   const response = await fetch(
     setOptions.baseUrl + setOptions.hostName + setOptions.path,
     {
@@ -11,54 +12,18 @@ async function pullData(user, reponame) {
       headers: setOptions.headers
     }
   );
-  // console.log("Response: ", response);
+
   const data = await response.json();
-  // console.log("Data:", data);
-
-  const output = outputApiData(data, user, reponame);
-
+  const output = await outputApiData(data, user, reponame);
   return output;
 }
 
 async function outputApiData(pulldata, user, reponame) {
   const output = await Promise.all(
     pulldata.reverse().map(async element => {
-      // let startPage = 1;
-      // const commitsPerPage = 1;
-      // let numberOfCommits = 0;
-      // let commitData = [0];
 
       const commitCount = await getCommitCount(element, user, reponame);
       console.log("CommitCount: ", commitCount);
-
-      // while (commitData.length > 0) {
-      //   const commitOptions = options(
-      //     "/repos/" +
-      //       user +
-      //       "/" +
-      //       reponame +
-      //       "/pulls/" +
-      //       element.number +
-      //       "/commits?per_page=" +
-      //       commitsPerPage +
-      //       "&page=" +
-      //       startPage
-      //   );
-
-      //   const getCommitData = await fetch(
-      //     commitOptions.baseUrl + commitOptions.hostName + commitOptions.path,
-      //     {
-      //       method: "GET",
-      //       headers: commitOptions.headers
-      //     }
-      //   );
-
-      //   commitData = await getCommitData.json();
-      //   console.log("Length: ", commitData.length);
-      //   numberOfCommits += commitData.length;
-      //   console.log("After: ", numberOfCommits);
-      //   startPage += 1;
-      // }
 
       return {
         id: element.id,
@@ -101,9 +66,7 @@ async function getCommitCount(element, user, reponame) {
     );
 
     commitData = await getCommitData.json();
-    console.log("Length: ", commitData.length);
     numberOfCommits += commitData.length;
-    console.log("After: ", numberOfCommits);
     startPage += 1;
   }
   return numberOfCommits;
